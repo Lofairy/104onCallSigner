140f1a5ba10272c9fd00cbbd42ac1d3640ffb0b6
Lofairy <vincent.lin@104.com.tw>
2018-08-02 16:39:12 +0800

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+require('chromedriver');
+
+const {Builder, By, until, Key} = require('selenium-webdriver');
+
+let autoOncallSign = async () => {
+    let driver = new Builder()
+        .forBrowser('chrome')
+        .build();
+
+    await driver.get('http://sysblog.104.com.tw/index.php');
+    await driver.findElement(By.id('portalid')).sendKeys('vincent.lin');
+    await driver.findElement(By.id('pwd')).sendKeys('...your pw');
+    await driver.findElement(By.id('button')).click();
+    await driver.wait(until.elementLocated(By.css('#left > dl:nth-child(4) > dt.checkin_dev > a')), 10 * 1000);
+    await driver.findElement(By.css('#left > dl:nth-child(4) > dt.checkin_dev > a')).click();
+    await driver.wait(until.elementLocated(By.css('input[name="aaa"][type=button][value="人員簽到"]')), 10 * 1000);
+    await driver.findElement(By.css('input[name="aaa"][type=button][value="人員簽到"]')).click();
+    await driver.sleep(2 * 1000);
+    driver.quit();
+};
+autoOncallSign();
